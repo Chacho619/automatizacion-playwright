658a3ea5e66365232c1aa48f5d420a827d11bcfb
David Gutierrez Trejo <48695896+Chacho619@users.noreply.github.com>
2026-05-26 19:12:36 -0600

    Fix CI final correcto

--- tests/registro-async.spec.js
+++ tests/registro-async.spec.js
@@ -1,14 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
 test('Registro de usuario asincrono', async ({ page }) => {
-
-  // Ir a página pública
   await page.goto('https://example.com');
 
-  // ✅ Polling (lo importante)
   await expect(page.locator('h1')).toContainText('Example Domain', {
-    timeout: 10000
+    timeout: 10000,
   });
-
 });
-``
